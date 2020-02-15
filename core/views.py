4f45b8569d82b512e2f234d5710b7b09a2c9d6ca
tempCross <bt.will777@gmail.com>
2020-02-15 18:52:27 -0500

    checkout temp

--- core/views.py
+++ core/views.py
@@ -8,3 +8,6 @@ def item_list(request):
         'items': Item.objects.all()
     }
     return render(request, "home-page.html", context)
+
+def checkout(request):
+    return render(request, "checkout-page.html")
